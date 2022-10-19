68c70163b87b4f78735565bcbfaf94bc9d52b95b
ooka <ookapratama2@gmail.com>
2022-10-19 12:37:37 +0800

    add splash screen and login act

--- app/src/main/java/com/example/alarm_kampus/MainActivity.java
+++ app/src/main/java/com/example/alarm_kampus/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.alarm_kampus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int durasi = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent login = new Intent(MainActivity.this, loginActivity.class);
+                startActivity(login);
+                finish();
+            }
+        }, durasi);
+
     }
 }
