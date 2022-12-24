b3abbc4d0938cf69ebf9fb8961fac0c1dc32fbdc
ooka <ookapratama2@gmail.com>
2022-12-24 20:50:54 +0800

    buat view setting (selesai)

--- app/src/main/java/com/example/alarm_kampus/Setting.java
+++ app/src/main/java/com/example/alarm_kampus/Setting.java
@@ -2,7 +2,10 @@ package com.example.alarm_kampus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Setting extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class Setting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        ImageView imageView;
+        imageView = findViewById(R.id.back);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Setting.this, HomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
